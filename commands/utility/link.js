cd546e48d479ec585c00fd40bb3cf0428258e86a
Benjamin Feller <benjamin@konect-group.fr>
2024-03-23 13:32:50 +0100

    feat(cmd): add campaign id option to /link

--- commands/utility/link.js
+++ commands/utility/link.js
@@ -1,12 +1,20 @@
 const { SlashCommandBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
 
+// /^[\\p{Ll}\\p{Lm}\\p{Lo}\\p{N}\\p{sc=Devanagari}\\p{sc=Thai}_-]+$/u.test($setName)
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('link')
-		.setDescription('Link a msg with a form'),
+		.setDescription('Link a msg with a form')
+		.addStringOption(option =>
+			option
+				.setName('campaign-id')
+				.setDescription('ID identifying which campaign url to search')
+				.setRequired(true),
+		),
 	async execute(interaction) {
 		const registerButton = new ButtonBuilder()
-			.setCustomId(`register-${interaction.id}`)
+			.setCustomId(`register-${interaction.options.getString('campaign-id')}`)
 			.setLabel('S\'inscrire')
 			.setStyle(ButtonStyle.Primary);
 
@@ -14,9 +22,7 @@ module.exports = {
 			.addComponents(registerButton);
 
 		const channel = await interaction.client.channels.fetch(interaction.channel.id);
-		await channel.send({
-			components: [row],
-		});
+		await channel.send({ components: [row] });
 
 		await interaction.reply({ content: 'Done.', ephemeral: true });
 		await interaction.deleteReply();
